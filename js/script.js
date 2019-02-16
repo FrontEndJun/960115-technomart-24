4c3983d250272eefe665890acb21df8f0a624521
Гоша <losetown343@e1.ru>
2019-02-16 11:50:19 +0500

    Обновление от 016.02 20:00 мск

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+function openMap() {
+  document.getElementById('popup-map').style.display = 'flex';
+}
+function closeMap() {
+  document.getElementById('popup-map').style.display = 'none';
+}
+
+function openFeedback() {
+  document.getElementById('feedback').style.display = 'flex';
+}
+function closeFeedback() {
+  document.getElementById('feedback').style.display = 'none';
+}
