073eff143a9091877ec8ae70514b038cd501bed3
Гоша <losetown343@e1.ru>
2019-02-27 20:27:02 +0500

    Обновление от 27.02 20:00 мск

--- js/script.js
+++ js/script.js
@@ -1,14 +1,48 @@
+let slides = document.getElementsByName('promo-slider');
+let currentSlide = 0;
+let buttonBuy = document.querySelectorAll('.action-buy');
 
 function openMap() {
-  document.getElementById('popup-map').style.display = 'flex';
+  document.querySelector('.popup-map').style.display = 'block';
 }
 function closeMap() {
-  document.getElementById('popup-map').style.display = 'none';
-}
+  document.querySelector('.popup-map').style.display = 'none';
 
+}
 function openFeedback() {
-  document.getElementById('feedback').style.display = 'flex';
+  document.querySelector('.modal-write-us').style.display = 'block';
 }
 function closeFeedback() {
-  document.getElementById('feedback').style.display = 'none';
+  document.querySelector('.modal-write-us').style.display = 'none';
+}
+
+for (let i = 0; i < buttonBuy.length; i++){
+  let btn = buttonBuy[i];
+  btn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    document.querySelector('.modal-order').style.display = 'block';
+  })
+}
+
+function closeModalOrder() {
+  document.querySelector('.modal-order').style.display = 'none';
+}
+
+function check() {
+  for(let i =0; i < slides.length; i++){
+    if(slides[i].checked) currentSlide = i;
+  }
+
+}
+
+function nextSlide() {
+  check();
+  if(++currentSlide >= slides.length) currentSlide = 0;
+  slides[currentSlide].checked = true;
+}
+
+function prevSlide() {
+  check();
+  if(--currentSlide < 0) currentSlide = slides.length - 1;
+  slides[currentSlide].checked = true;
 }
